397c44d7674cabd2c72e44d5e182192bdabb1183
zuleika <njokizuleika@gmail.com>
2023-04-04 14:47:11 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+handleToDo(e.target,new_todo.value);
 });
+});
+function handleToDo(todo){
+  let p = document.createElement("p")
+  let btn = document.createElement("button")
+  btn.textContents = "x"
+  p.textContents = todo;
+document.querySelector("#todo_container").appendChild(p);
+}
+var row = 0;
+function addText(){
+    var input = document.getElementById('inputTask').value;
+    if(input != "")
+    {
+    var node=document.createElement("p");
+    var textnode=document.createTextNode(input);
+    node.appendChild(textnode);
+        node.setAttribute("id","contentP"+row);
+    document.getElementById('addTask').appendChild(node);
+
+    var removeTask = document.createElement('input');
+    removeTask.setAttribute('type', 'button');
+    removeTask.setAttribute("value", "Remove");
+    removeTask.setAttribute("id", "removeButton");
+    removeTask.setAttribute("onClick", "deleterow("+ row +");");
+
+    node.appendChild(removeTask);
+        row++;
+    }
+    else
+    {
+        alert("Please insert a value!");
+    }
+
+}
+function deleterow(ID)
+{
+    document.getElementById('contentP'+ID).remove();
+}
